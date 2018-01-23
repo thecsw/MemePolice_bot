10ae1b552fbea81f0450f7a6895e81f031ca2dec
thecsw <thecsmoothiew@yahoo.com>
2018-01-24 01:00:26 +0400

    Working on enhancements

--- pewds.py
+++ pewds.py
@@ -16,29 +16,22 @@ reddit = praw.Reddit(client_id = config.client_id,
 
 subreddit = reddit.subreddit('PewdiepieSubmissions')
 
-banned = ["399","chair","tide","pods","do you know","but can you do this","the way","uganda","noodle","but can","slippy","you do this"]
+banned = ["399","chair","tide","pods","do you know","but can you do this","the way","uganda","noodle","but can","slippy","you do this","skiddadle","skadoodle","upvote if"]
 
 websites = ["youtube","youtu.be","gallery","gif","mp4","webm","gfycat"]
 
-message = """ *Beep Boop and police sounds*
+message = """ *Police sounds*
 
-Good day, sir! You have been visited by meme police!
+Good day, sir! You have been visited by the **Meme Police!**
 
-I see that you posted an illegal meme in r/PewdiepieSubmissions, for example: chair memes, tide pods, uganda memes and etc.
+I see that you posted an illegal meme on r/PewdiepieSubmissions, *for example: chair memes, tide pods, uganda memes, skiddadle skadoodle, and etc.*
 
-Do not publish old or overused memes here because only fresh and scrattar memes are allowed!
+Do not publish old or overused memes here because **only fresh and scrattar memes are allowed!**
 
-You have been warned and please, do not make the same mistake twice.
-
-And also, I am giving you a penalty of 1 downvote.
-
+You have been warned and please, do not make the same mistake twice. No penalty for now.
 ______________________
 
-Hello there! I am a new AI bot in this subreddit and my duty is to keep order and peace.
-I do not allow old and overused memes.
-I use advanced and cutting-edge technologies to detect stale memes and will report them to the administration.
-If you have any suggestions, reply to my bot and I will read them!
-Meme police is real.
+^(Hello there! I am a new AI bot in this subreddit and my duty is to keep order and peace. I do not allow old and overused memes. Meme Police is real.)
 """
 
 def ban(post, text, desc):
@@ -48,14 +41,14 @@ def ban(post, text, desc):
            post.reply(message)
 #           post.downvote()
            print("Will w8 5 mins")
-           time.sleep(600)
+           time.sleep(300)
            return
        if word in desc:
            print("Found an illegal meme!")
            post.reply(message)
 #           post.downvote()
            print("Will w8 5 mins")
-           time.sleep(600)
+           time.sleep(300)
            return     
 
 def detect(post):
@@ -69,17 +62,18 @@ def detect(post):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         gray = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)[1]
         #gray = cv2.medianBlur(gray, 3)
-        filename = "test.png"
+        filename = "{}-TEST.png".format(im)
         cv2.imwrite(filename, gray)
-        img = Image.open("test.png")
+        img = Image.open(filename)
         text = pytesseract.image_to_string(img).lower()
         os.remove(filename)
         os.remove(im)
         print(text)
         ban(post, text, desc)            
 
-for submission in subreddit.stream.submissions():
-    time.sleep(2)
+#for submission in subreddit.stream.submissions():
+#for submission in subreddit.top('day', limit=25):
+for submission in subreddit.hot():
     post = reddit.submission(submission)
     if "gallery" in post.url:
         continue
@@ -103,10 +97,14 @@ for submission in subreddit.stream.submissions():
         continue
     if "imgur" in post.url:
         continue
-
+    if "twitter" in post.url:
+        continue
+    if "instagram" in post.url:
+        continue
     
-    if "png" or "jpg" in post.url:
+    if "png" in post.url:
         detect(post)
         continue
-    else:
+    elif "jpg" in post.url:
+        detect(post)
         continue
