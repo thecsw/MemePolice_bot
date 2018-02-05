c90af433cc7c8615c3cb74c805d1458ce8288ac7
Sagindyk <sagindyk.urazayev@protonmail.ch>
2018-02-05 20:44:03 +0400

    Because of API restrictions, need to wait 1 minute approx. for the cooldown

--- src/__main__.py
+++ src/__main__.py
@@ -3,6 +3,7 @@ from message import message
 from text_recognition import text_recognition
 from blacklist import illegal_memes
 import config as config
+import time
 
 from tqdm import tqdm
 import praw
@@ -23,19 +24,20 @@ pattern = re.compile(".(jpe?g|png|gifv?)(\?\S*)?")
 def ban(post):
     print("Found an illegal title!")
     post.reply(message)
+    time.sleep("60")
 
 
 if __name__ == "__main__":
     while True:
         for submission in subreddit.stream.submissions():
             post = reddit.submission(submission)
-            title = post.title
+            title = post.title.encode('utf-8').lower()
             print("Title: {}".format(title))
 
             if pattern.search(post.url) is not None:
                 print("\tMatched regex, post is an image")
                 try:
-                    meme_text = text_recognition(post)
+                    meme_text = text_recognition(post).lower()
                 except:
                     continue
                 print("\tImage Text: {}".format(meme_text))
